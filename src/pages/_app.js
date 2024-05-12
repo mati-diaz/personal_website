8afd397721f0324279c1f6cd056900f6978a9566
Matias Diaz <matidiazruiz42@gmail.com>
2024-05-12 02:17:02 -0300

    .

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -11,15 +11,14 @@ export default function App({ Component, pageProps, router }) {
 
             <AnimatePresence
                 mode="wait"
-                initial={false}
                 onExitComplete={() => window.scrollTo(0, 0)}
             >
                 <motion.div
                     key={router.route}
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
-                    // exit={{ opacity: 0 }}
-                    transition={{ duration: 0.2, type: 'keyframes' }}
+                    exit={{ opacity: 0 }}
+                    transition={{ duration: 0.2 }}
                     className={styles.mainContent}
                 >
                     <Component {...pageProps} key={router.route} />
