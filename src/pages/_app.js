a5eb9055bd45b1d3f6f94743e45c2e2d71df7dde
Matias Diaz <matidiazruiz42@gmail.com>
2024-05-12 01:07:22 -0300

    .

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,38 @@
 import '@/styles/globals.css';
-import Layout from '@/components/Layout';
-import { AnimatePresence } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
+import NavBar from '@/components/NavBar';
+import styles from '@/styles/Layout.module.css';
 
 export default function App({ Component, pageProps, router }) {
+    const container = {
+        hidden: { opacity: 0 },
+        show: {
+            opacity: 1,
+        },
+        exit: { opacity: 0 },
+    };
+
     return (
-        <AnimatePresence
-            mode="wait"
-            initial={false}
-            onExitComplete={() => window.scrollTo(0, 0)}
-        >
-            <Layout route={router.route}>
-                <Component {...pageProps} key={router.route} />
-            </Layout>
-        </AnimatePresence>
+        <>
+            <NavBar />
+
+            <AnimatePresence
+                mode="wait"
+                initial={false}
+                onExitComplete={() => window.scrollTo(0, 0)}
+            >
+                <motion.div
+                    key={router.route}
+                    initial="hidden"
+                    animate="show"
+                    exit="exit"
+                    variants={container}
+                    transition={{ duration: .2, type: "keyframes" }}
+                    className={styles.mainContent}
+                >
+                    <Component {...pageProps} key={router.route} />
+                </motion.div>
+            </AnimatePresence>
+        </>
     );
 }
