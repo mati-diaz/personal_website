cdb76f8aa0917305e9267bd18710e8ac34b79daf
Matias Diaz <matidiazruiz42@gmail.com>
2024-05-12 01:27:35 -0300

    .

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,24 +2,13 @@ import '@/styles/globals.css';
 import { AnimatePresence } from 'framer-motion';
 
 export default function App({ Component, pageProps, router }) {
-    const container = {
-        hidden: { opacity: 0 },
-        show: {
-            opacity: 1,
-        },
-        exit: { opacity: 0 },
-    };
-
     return (
-        <>
-            <AnimatePresence
-                mode="wait"
-                initial={false}
-                onExitComplete={() => window.scrollTo(0, 0)}
-            >
-                {/* <Component {...pageProps} key={router.route} /> */}
-                <Component {...pageProps} key={router.asPath} />
-            </AnimatePresence>
-        </>
+        <AnimatePresence
+            mode="wait"
+            initial={false}
+            onExitComplete={() => window.scrollTo(0, 0)}
+        >
+            <Component {...pageProps} key={router.route} />
+        </AnimatePresence>
     );
 }
