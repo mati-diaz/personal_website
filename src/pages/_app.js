79ce701eaf0a751b3638819dbaffa9208926d0c6
Matias Diaz <matidiazruiz42@gmail.com>
2024-05-12 01:33:37 -0300

    .

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,19 @@
+import NavBar from '@/components/NavBar';
 import '@/styles/globals.css';
 import { AnimatePresence } from 'framer-motion';
 
 export default function App({ Component, pageProps, router }) {
     return (
-        <AnimatePresence
-            mode="wait"
-            initial={false}
-            onExitComplete={() => window.scrollTo(0, 0)}
-        >
-            <Component {...pageProps} key={router.route} />
-        </AnimatePresence>
+        <>
+            <NavBar />
+
+            <AnimatePresence
+                mode="wait"
+                initial={false}
+                onExitComplete={() => window.scrollTo(0, 0)}
+            >
+                <Component {...pageProps} key={router.route} />
+            </AnimatePresence>
+        </>
     );
 }
