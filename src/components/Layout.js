6b0d9a9418ebd11beb412a70c034ad07bc415f7c
Matias Diaz <matidiazruiz42@gmail.com>
2024-05-15 09:02:01 -0300

    Fix Font

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,16 +1,24 @@
 import { motion } from 'framer-motion';
 import React, { useState } from 'react';
+import { Open_Sans } from 'next/font/google';
+import NavBar from './NavBar';
+
+const openSans = Open_Sans({ subsets: ['latin'] });
 
 export default function Layout({ children }) {
     return (
-        <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.3 }}
-            className="mainContent"
-        >
-            {children}
-        </motion.div>
+        <div className={openSans.className}>
+            <NavBar />
+
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.3 }}
+                className="mainContent"
+            >
+                {children}
+            </motion.div>
+        </div>
     );
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,13 @@
-'use client'
-import NavBar from '@/components/NavBar';
 import '@/styles/globals.css';
 import { AnimatePresence } from 'framer-motion';
 
 export default function App({ Component, pageProps, router }) {
     return (
-        <>
-            <NavBar />
-
-            <AnimatePresence
-                mode="wait"
-                onExitComplete={() => window.scrollTo(0, 0)}
-            >
-                <Component {...pageProps} key={router.route} />
-            </AnimatePresence>
-        </>
+        <AnimatePresence
+            mode="wait"
+            onExitComplete={() => window.scrollTo(0, 0)}
+        >
+            <Component {...pageProps} key={router.route} />
+        </AnimatePresence>
     );
 }
